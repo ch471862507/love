e582d046cddb2422353317b7de70911a4a591f6d
ch471862507 <471862507@qq.com>
2019-05-13 14:44:41 +0800

    love

---
+++ dbconfig.js
@@ -0,0 +1,33 @@
+var fs = require("fs");
+
+var mysql = require("mysql");
+
+var pool = Object;
+
+async function initPool() {
+	await fs.exists("dbconfig.json", function(exists) {
+		if (exists) {
+			pool = mysql.createPool(
+				JSON.parse(fs.readFileSync("dbconfig.json"))
+			);
+		} else {
+			pool = mysql.createPool({
+				host: "localhost",
+				user: "user",
+				password: "password",
+				database: "database",
+				port: 3306
+			});
+		}
+	});
+}
+
+initPool();
+
+function query(sql, callback) {
+	pool.query(sql, function(err, rows) {
+		callback(err, rows);
+	});
+}
+
+exports.query = query;
